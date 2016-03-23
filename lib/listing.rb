5ae989a230aa33e6dcc458bd0dadb98a94dc7c2f
Duy Khoa <duykhoa12t@gmail.com>
2016-03-24 02:34:01 +0800

    Change AL for better something ... :trollface:

--- lib/listing.rb
+++ lib/listing.rb
@@ -6,24 +6,6 @@ class Listing
   end
 
   def build
-    result = [ array ]
-    while result.size < total_mutations do
-      a_mutate = mutate
-      result << a_mutate unless result.include?(a_mutate)
-    end
-
-    result
-  end
-
-  def total_mutations
-    (1..array.size).reduce(:*)
-  end
-
-  def mutate
-    dup = array
-    begin
-      dup = array.shuffle
-    end while dup == array
-    dup
+    array.permutation.to_a
   end
 end
--- spec/listing_spec.rb
+++ spec/listing_spec.rb
@@ -25,23 +25,4 @@ RSpec.describe Listing do
       it { expect(subject).to include [3,4,5,2,1] }
     end
   end
-
-  describe "#mutate" do
-    let(:input_array) { [1,2,3] }
-    subject { described_class.new(array: input_array).mutate }
-    it { is_expected.to be_a Array }
-    # bay gio viet test de check array nay phai la bien the cua input_array
-    it { expect(subject).to include (1) }
-    it { expect(subject).to include (2) }
-    it { expect(subject).to include (3) }
-
-    # egh, can't equal input_array leh
-    it { is_expected.not_to eq input_array }
-  end
-
-  describe "#total_mutation" do
-    let(:input_array) { [1,2,3] }
-    subject { described_class.new(array: input_array).total_mutations }
-    it { is_expected.to eq 6 }
-  end
 end
